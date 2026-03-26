35b0a2cb72a68185aebaac0364a15c0b66a0341f
arobbin2 <arobbin2@nd.edu>
2026-03-26 18:40:19 +0000

    bases pngs

--- src/scoreboard.py
+++ src/scoreboard.py
@@ -552,9 +552,9 @@ class LEDScoreboard:
             if "LOADED" in bases_text:
                 bases_key = "123"
             else:
-                on_first = "1B" in bases_text
-                on_second = "2B" in bases_text
-                on_third = "3B" in bases_text
+                on_first = "1--" in bases_text
+                on_second = "-2-" in bases_text
+                on_third = "" in bases_text
                 bases_key = ("1" if on_first else "-") + ("2" if on_second else "-") + ("3" if on_third else "-")
 
             compact_bases = bases_key
